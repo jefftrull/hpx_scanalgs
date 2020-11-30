bf30163e56cf12571773f66e3ea60ec27bb63ae0
Jeff Trull <edaskel@att.net>
2020-11-30 08:09:55 -0800

    Update for latest C++ and CMake versions
    For CMake:
    - add project()
    - remove conflicting hpx_setup_target/target_link_libraries usage
    For C++:
    - namespace my personal exclusive_scan() that is otherwise ambiguous
      thanks to ADL with std::

--- manual_parallel.cpp
+++ manual_parallel.cpp
@@ -113,6 +113,7 @@ std::pair<FwdIter2, T> exclusive_scan_mt(FwdIter1 start, FwdIter1 end, FwdIter2 
 
 std::size_t chunksize = 2500000;
 
+namespace jet {
 // chunk up the original data in cache-friendly sizes
 template<typename T, typename FwdIter1, typename FwdIter2, typename Op = std::plus<T>>
 FwdIter2 exclusive_scan(FwdIter1 start, FwdIter1 end, FwdIter2 dst, T init = T(), Op op = Op())
@@ -139,6 +140,8 @@ FwdIter2 exclusive_scan(FwdIter1 start, FwdIter1 end, FwdIter2 dst, T init = T()
     return dst;
 
 }
+}
+
 void verify()
 {
     // generate a bunch of random cases to show it works
@@ -157,7 +160,7 @@ void verify()
         std::generate(data.begin(), data.end(),
                       [&]() { return values_dist(mersenne_engine); });
         std::vector<int> par_result(sz);
-        exclusive_scan(data.begin(), data.end(), par_result.begin(), 1);
+        jet::exclusive_scan(data.begin(), data.end(), par_result.begin(), 1);
         std::vector<int> ser_result(sz);
         sequential_exclusive_scan(data.begin(), data.end(), ser_result.begin(), 1);
         assert(par_result == ser_result);
@@ -228,7 +231,7 @@ int main(int argc, char* argv[])
             thread_count = state.range(1);
             chunksize = state.range(2);
             for (auto _ : state) {
-                exclusive_scan(data.begin(), data.end(), result.begin(), 0);
+                jet::exclusive_scan(data.begin(), data.end(), result.begin(), 0);
                 benchmark::DoNotOptimize(result);
             }
         })->Apply(CustomArguments)->UseRealTime();
